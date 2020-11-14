2a6013c2584124157e29d15245a80f4cda6d817e
happiness123 <sukaina.ab220@gmail.com>
2020-11-14 16:16:27 +0100

    first  commit

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
 <!-- TODO Application entry point. Login view -->
+<?php
+session_start();
+if (isset($_SESSION["userName"]) && isset($_SESSION["uid"]) && isset($_SESSION["email"])) {
+    header("Location: src/dashboard.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/login.css?v=<?php echo time(); ?>">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/loginController.js"></script>
+    <title>Login - Foo</title>
+</head>
+
+<body>
+    <form class="form-signin" method="post" action="src/library/loginController.php">
+        <!--<img class="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">-->
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputUserName" class="sr-only">Username</label>
+        <input name="username" type="text" id="inputUserName" class="form-control" placeholder="Username" required="" autofocus="">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <div class="checkbox mb-3">
+            <label>
+                <input name="remember" type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <div class="mb-3">
+            <button class="btn" type="button" id="registerButton">Not Registered?</a>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <input type="text" value="login" name="method" class="hidden">
+    </form>
+
+</body>
+
+</html>
---
+++ src/library/loginController.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+//Check if post exist
+
+if (isset($_POST)) {
+    //   print_r($_POST);
+    if (isset($_POST["method"])) {
+        if ($_POST["method"] === "login") {
+            validateLogin($_POST["username"], $_POST["password"]);
+        } elseif ($_POST["method"] === "logout") {
+            logOut();
+        }
+    }
+}
+function validateLogin($userName, $password)
+{
+    $string = file_get_contents("../../resources/users.json");
+
+    $users = json_decode($string, true);
+
+    foreach ($users["users"] as $i => $user) {
+        if ($user["name"] === $userName) {
+            $pss = $user['password'];
+            if (password_verify($password, $pss)) {
+                $_SESSION["userName"] = $user["name"];
+                $_SESSION["uid"] = $users["userId"];
+                $_SESSION["email"] = $users["email"];
+                header("Location: ../dashboard.php");
+            }
+        }
+    }
+}
+function logOut()
+{
+    unset($_SESSION["userName"]);
+    unset($_SESSION["uid"]);
+    unset($_SESSION["email"]);
+    session_destroy();
+    header("Location: ../..");
+}
