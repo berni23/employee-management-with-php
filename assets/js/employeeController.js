8acaaf9715f0d08c0cb599b21b6d5f8546d66e99
happiness123 <sukaina.ab220@gmail.com>
2020-11-16 16:54:52 +0100

    Alert box fixed

--- assets/js/employeeController.js
+++ assets/js/employeeController.js
@@ -56,7 +56,14 @@ $(function() {
             contentType: false
         }).then((data) => {
             console.log(data)
-            window.open("dashboard.php", "_self");
+            $("#msgWrapper").empty().append(`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <p id="msg">Employee Successfully Saved.</p>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>`)
+
+
         });
     })
     document.getElementById("return").addEventListener("click", (e) => {
