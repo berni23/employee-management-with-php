d4646ff0248814a735e381c7ff97c9d919102586
bernat-ferrer <bernatferrerm@gmail.com>
2020-11-30 13:59:02 +0100

    env vars

--- config/constants.php
+++ config/constants.php
@@ -3,6 +3,16 @@
 
 <?php
 
+
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+
+// environtmental vars
+$dotenv->load();
+
 $root = getcwd();
 
 define('BASEPATH', $root);
---
+++ config/db.php
@@ -0,0 +1,17 @@
+
+
+
+<?php
+
+class db
+{
+
+    static $dbServername  = ${DB_SERVER_NAME};
+    static $dbUsername = "root";
+    static $dbPassword = "pass";
+    static $dbName = "employeeV2";
+
+
+
+    //static $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+}
