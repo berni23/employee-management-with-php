439cacf7f6176de4fefbe2c02991db183bcc0d89
bernat-ferrer <bernatferrerm@gmail.com>
2020-11-30 16:02:46 +0100

    db connection

--- index.php
+++ index.php
@@ -4,13 +4,10 @@ session_start();
 
 require_once 'config/constants.php';
 
-
 require_once 'includes/autoloader.php';
 spl_autoload_register('myAutoLoader');
 
-
 require_once 'controllers/login.php';
-
 
 // Returns a string if the URL has parameters or NULL if not
 
