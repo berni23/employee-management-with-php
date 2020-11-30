a6ddd508b973208a8071362b3d9b3f90ab1b9916
luis sastre san emeterio <luis@MBP-de-luis.home>
2020-11-30 17:10:32 +0100

    connection database

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ require_once 'includes/autoloader.php';
 spl_autoload_register('myAutoLoader');
 
 require_once 'controllers/login.php';
+require_once 'model/loginModel.php';
+
 
 // Returns a string if the URL has parameters or NULL if not
 
--- libs/classes/model.php
+++ libs/classes/model.php
@@ -7,11 +7,15 @@
 
 abstract class model
 {
+    public function __construct() {
+        $this->db = new Database;
+    }
 
-
+    /*
     abstract public function getById($id);
 
     abstract public function  setById($id);
 
     abstract public function someMethod3(): string;
+    */
 }
