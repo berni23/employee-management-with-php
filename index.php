750899570f6d9adce958c6f685bbf7a1b5be6eb4
bernat-ferrer <bernatferrerm@gmail.com>
2020-12-01 16:10:18 +0100

    paths html changed

--- index.php
+++ index.php
@@ -6,17 +6,14 @@ require_once 'config/constants.php';
 // require_once 'includes/autoloader.php';
 // spl_autoload_register('myAutoLoader');
 
-
 require_once  CLASSES . '/Database.php';
 require_once  CLASSES . '/controller.php';
 require_once  CLASSES . '/Model.php';
 require_once  CLASSES . '/view.php';
 
-
 require_once 'models/loginModel.php';
 require_once 'controllers/login.php';
 
 require_once  CLASSES . '/Router.php';
-
 
 new Router();
--- libs/classes/Router.php
+++ libs/classes/Router.php
@@ -6,44 +6,35 @@ class Router
 {
 
     private $url;
-
     public function __construct()
     {
         session_start();
-        //  unset($_SESSION['userName']);
-
+        // unset($_SESSION['userName']);
         if (!isset($_SESSION['userName'])) new login();
         else  $this->route();
     }
 
     private function urlParams()
     {
-
         $url = isset($_GET['url']) ? $_GET['url'] : null;
         $url = rtrim($url, '/');
         $url = explode('/', $url);
-
         $this->url = $url;
     }
 
     private function getController(): string
     {
         $this->urlParams();
-
         return 'controllers/' . $this->url[0] . '.php';
     }
     private function route(): void
     {
 
         echo 're-routing..';
-
         $fileController = $this->getController();
-
         if (file_exists($fileController)) {
             require_once $fileController;
-
             $controller = new $this->url[0];
-
             if (isset($this->url[1]))   $controller->{$this->url[1]}();
         } else {
             // new error()
