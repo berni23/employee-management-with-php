85bb8733a5af63c16355465539cfcc9cb6ab7459
luis sastre san emeterio <luis@MBP-de-luis.home>
2020-12-02 12:25:17 +0100

    insert finish

--- models/dashboardModel.php
+++ models/dashboardModel.php
@@ -17,13 +17,27 @@ class dashboardModel extends Model
 
     public function insertEmployee($data)
     {
-        $pdo = $this->db->connect();
+        try{
+            $pdo = $this->db->connect();
 
         
-        $sql = "INSERT INTO employee (name,email,age,streetAddress,city,state,postalCode,phoneNumber) 
+            $sql = "INSERT INTO employees (name,email,age,streetAddress,city,state,postalCode,phoneNumber) 
                 VALUES (:name,:email,:age,:streetAddress,:city,:state,:postalCode,:phoneNumber)";
-        $stmt= $pdo->prepare($sql);
-        return $stmt->execute($data);
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute(['name' => $data['name'], 
+                            'email' => $data['email'], 
+                            'age' => $data['age'], 
+                            'streetAddress' => $data['streetAddress'],
+                            'city' => $data['city'],
+                            'state' => $data['state'],
+                            'postalCode' => $data['postalCode'],
+                            'phoneNumber' => $data['phoneNumber']]);
+
+            return true;
+        }catch(PDOException $e) {
+            echo 'Ya existe el empleado';
+            return false;
+        }
     }
 
     
