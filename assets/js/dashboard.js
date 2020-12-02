85bae09079f0bc7617f4b7c5c64c6ba6756b5c27
luis sastre san emeterio <luis@MBP-de-luis.home>
2020-12-02 12:25:17 +0100

    insert finish

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -35,7 +35,10 @@ var gridObject = {
             return fetch(BASE_URL + "/dashboard/insertEmployees", {
                 method: 'POST',
                 body: JSON.stringify(item)
-            }).then(res => res.text()).then(res => load())
+            }).then(res => res.text()).then(function(res){ 
+                console.log(res); 
+                load()
+            });
         },
 
         // function to update data
